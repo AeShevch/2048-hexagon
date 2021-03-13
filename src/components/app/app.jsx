21322e1fdd53b3ca54e22e552d752b7d797e116f
AeShevch <shevch.ae@gmail.com>
2021-03-13 18:46:37 +0300

    initial

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import API from "../../api/api";
 import Board from "../board/board";
 
-// const SERVER_URL = `//68f02c80-3bed-4e10-a747-4ff774ae905a.pub.instances.scw.cloud`;
-const SERVER_URL = `http://localhost:13337`;
+const SERVER_URL = `//68f02c80-3bed-4e10-a747-4ff774ae905a.pub.instances.scw.cloud`;
+// const SERVER_URL = `http://localhost:13337`;
 const api = new API(SERVER_URL);
 
 const newCells = api.getNewCellsForGameLevel(2);
