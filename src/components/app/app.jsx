64860b4d90c957c632cb6975ed22bd0eaae39023
ae.shevch <shevch.ae@gmail.com>
2021-03-15 15:25:50 +0300

    refactor: moves servers list to api folder

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,24 +4,12 @@ import API from "../../api/api";
 import Board from "../board/board";
 import Settings from "../settings/settings";
 import {generateInitialBoardData} from "../utils/utils";
+import servers from "./servers";
 
 const App = () => {
-
-
   const [level, setLevel] = useState(0);
   const [cells, setCells] = useState([]);
-  const [backendServers, setSelectedBackendServer] = useState([
-    {
-      id: `remote`,
-      value: `//68f02c80-3bed-4e10-a747-4ff774ae905a.pub.instances.scw.cloud`,
-      isSelected: true,
-    },
-    {
-      id: `localhost`,
-      value: `http://localhost:13337`,
-      isSelected: false,
-    },
-  ]);
+  const [backendServers, setSelectedBackendServer] = useState(servers);
 
   const getUpdatedCells = (currentCells, newCells) => {
     if (!currentCells.length) currentCells = generateInitialBoardData(level);
---
+++ src/components/app/servers.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: `remote`,
+    value: `//68f02c80-3bed-4e10-a747-4ff774ae905a.pub.instances.scw.cloud`,
+    isSelected: true,
+  },
+  {
+    id: `localhost`,
+    value: `http://localhost:13337`,
+    isSelected: false,
+  },
+]
